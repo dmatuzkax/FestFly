aa3ee872726fde03a43744fe4add3605f9aedf71
Daniel Matuzka <dmatuzka@bu.edu>
2024-06-10 20:06:50 -0400

    First version with Ticketmaster API to be connected to OAuth

---
+++ client/src/components/Cards.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
+
+function Cards() {
+
+  const [events, setEvents] = useState([]);
+
+
+    useEffect(() => {
+        fetch('http://localhost:3001')
+            .then(response => response.json())
+            .then(data => {
+                setEvents(data);
+            })
+            .catch(error => {
+                console.error('Error fetching concert data:', error);
+            });
+    }, []);
+
+    return (
+      // <div>
+      //     {events.length != 0 ? events.map((concert, index) => {
+      //       <ConcertCard concert={concert} />
+      //     })[1] : <p>No Events Set For This Artist </p>}
+      // </div>
+      <div className='cards'>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          {events.length !== 0 ? (
+            <ul className="cards__items">
+              {events.map((concert, index) => (
+                <ConcertCard key={index} concert={concert} />
+              ))}
+            </ul>
+          ) : (
+            <p>No Events Set For This Artist</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+
+  function ConcertCard({ concert }) {
+    const { name, dates, _embedded, images } = concert;
+    const venue = _embedded.venues[0].name;
+    const date = dates.start.localDate;
+    const time = dates.start.localTime;
+    const city = _embedded.venues[0].city.name;
+    const country = _embedded.venues[0].country.name;
+    const src = images[0].url;
+
+    return(
+      <li className='cards__item'>
+        <CardItem  src = {src} name = {name} date = {date} time = {time} venue = {venue} city = {city} country = {country} />
+      </li>
+      
+    );
+
+    
+  }
+
+}
+
+export default Cards
